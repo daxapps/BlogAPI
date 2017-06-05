496684bd4539c32ff2fc7055c0adb59c52a4934b
Jason Crawford <daxapps777@gmail.com>
2017-06-04 19:23:58 -0500

    Create PUT endpoints

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -32,6 +32,34 @@ router.post('/', jsonParser, (req, res) => {
   res.status(204).end();
 });
 
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    author: req.body.author,
+    content: req.body.content,
+    publishDate:req.body.publishDate
+  });
+  res.status(204).end();
+});
+
   const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
